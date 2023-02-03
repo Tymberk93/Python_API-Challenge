923bd35b8c4612f9e226c3121cd5c88bb2a79460
Tymber Hamilton <tymberk@Tymbers-MacBook-Pro.local>
2023-02-02 23:43:03 -0600

    api keys

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,7 @@
+# Adding config.py file.
+
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "2b6ea4662b345d116d241674086e6188"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "a8fc96adaefa4adf8c7ae4ccd764d467"
